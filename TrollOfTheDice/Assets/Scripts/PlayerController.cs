11125b7f0064d3ff50ca66440c8d5bd1ed38a266
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-07-15 16:09:47 -0400

    Added basic movement

--- TrollOfTheDice/Assets/Scripts/PlayerController.cs
+++ TrollOfTheDice/Assets/Scripts/PlayerController.cs
@@ -13,4 +13,30 @@ public class PlayerController : MonoBehaviour {
         transform.position = levelGrid.CellToWorld(gridPosition);
         Debug.Log("New world position: " + transform.position);
     }
+
+    private void Update() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // temporary movement code
+        if(Input.GetKeyDown(KeyCode.W)) {
+            move(0, 1);
+        }
+        if(Input.GetKeyDown(KeyCode.S)) {
+            move(0, -1);
+        }
+        if(Input.GetKeyDown(KeyCode.A)) {
+            move(-1, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.D)) {
+            move(1, 0);
+        }
+    }
+
+    private void move(int x, int z) {
+        gridPosition.x += x;
+        gridPosition.z += z;
+
+        transform.position = levelGrid.CellToWorld(gridPosition);
+    }
 }
