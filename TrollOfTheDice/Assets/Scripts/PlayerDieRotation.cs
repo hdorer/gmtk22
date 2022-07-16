08f8cb1025d078e33f7f926ad2718be3f9041ea7
Max Cohen <max.cohen@mymail.champlain.edu>
2022-07-15 23:26:58 -0400

    rotation tiles functional!

--- TrollOfTheDice/Assets/Scripts/PlayerDieRotation.cs
+++ TrollOfTheDice/Assets/Scripts/PlayerDieRotation.cs
@@ -23,6 +23,6 @@ public class PlayerDieRotation : MonoBehaviour {
         return 0;
     }
 
-    public void RotateClockwise() { transform.Rotate(0, 0, 90, Space.Self); }
-    public void RotateCounterClockwise() { transform.Rotate(0, 0, -90, Space.Self); }
+    public void RotateClockwise() { transform.Rotate(0, 90, 0, Space.World); }
+    public void RotateCounterClockwise() { transform.Rotate(0, -90, 0, Space.World); }
 }
