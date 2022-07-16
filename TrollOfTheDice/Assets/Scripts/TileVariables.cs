ae5baf4e09d77576457120c7ca9b8de32acf24c2
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-07-16 17:10:37 -0400

    Got the tile color change to work in level 10

--- TrollOfTheDice/Assets/Scripts/TileVariables.cs
+++ TrollOfTheDice/Assets/Scripts/TileVariables.cs
@@ -9,25 +9,29 @@ public class TileVariables : GridAligned
     [SerializeField] private bool isActive;
     [SerializeField] private bool isPermanent;
     [SerializeField] private Tilemap tilemap;
-    [SerializeField] private Tile activeTile;
-    [SerializeField] private Tile inactiveTile;
+    [SerializeField] private Sprite[] activeSprites;
+    [SerializeField] private Sprite[] inactiveSprites;
     public Vector2Int GridPosition { get { return gridPosition; } }
     public bool IsActive { get { return isActive; } }
 
     public void OnTriggerEnter(Collider collision)
     {
         int dieFace = collision.gameObject.GetComponent<PlayerDieRotation>().getActiveFace();
-        Vector3Int currentCell = new Vector3Int(GridPosition.x,GridPosition.y - 1,0);
+        Vector3Int currentCell = new Vector3Int(GridPosition.x, GridPosition.y - 1, 0);
+
+        Tile tile = ScriptableObject.CreateInstance<Tile>();
 
         if (dieFace == victoryNum)
         {
-            tilemap.SetTile(currentCell, activeTile);
+            tile.sprite = activeSprites[victoryNum - 1];
+            tilemap.SetTile(currentCell, tile);
             isActive = true;
             levelGrid.GetComponent<VictoryCheck>().CheckIfWin();
         }
         else if (!isPermanent)
         {
-            tilemap.SetTile(currentCell, inactiveTile);
+            tile.sprite = inactiveSprites[victoryNum - 1];
+            tilemap.SetTile(currentCell, tile);
             isActive = false;
         }
     }
