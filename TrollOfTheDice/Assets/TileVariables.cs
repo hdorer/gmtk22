c29d795513d3b3f3b746ed2245db9bebdf503339
Max Cohen <max.cohen@mymail.champlain.edu>
2022-07-15 20:21:48 -0400

    can now move to new scene when all are collided (needs testing)

--- TrollOfTheDice/Assets/TileVariables.cs
+++ TrollOfTheDice/Assets/TileVariables.cs
@@ -8,10 +8,13 @@ public class TileVariables : GridAligned
     [SerializeField] private bool isActive;
     [SerializeField] private bool isPermanent;
     public Vector2Int GridPosition { get { return gridPosition; } }
+    public bool IsActive { get { return isActive; } }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter(Collider collision)
     {
         int dieFace = collision.gameObject.GetComponent<PlayerDieRotation>().getActiveFace();
+
+        Debug.Log(dieFace);
 
         if (dieFace == victoryNum) { isActive = true; }
         else if (!isPermanent) { isActive = false; }
