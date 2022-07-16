21d1b3fa35f65fae87f22f8a2d9425f52e654d58
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-07-16 01:47:08 -0400

    Added base class/interface

---
+++ TrollOfTheDice/Assets/Scripts/MoveState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+// This class kinda just exists to be used as a parameter in the interface
+
+public class MoveState {
+    public MoveState() {
+
+    }
+}
