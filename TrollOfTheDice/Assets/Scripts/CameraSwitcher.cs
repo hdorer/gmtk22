46c2df482541e563f43307c5d70ed9ee284f5e7c
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-07-17 00:47:06 -0400

    Clicking left and right mouse click switches between the four cameras in the scene

--- TrollOfTheDice/Assets/Scripts/CameraSwitcher.cs
+++ TrollOfTheDice/Assets/Scripts/CameraSwitcher.cs
@@ -1,14 +1,47 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour {
+    [SerializeField] CinemachineVirtualCamera[] cameras;
+    int currentCamera = 0;
+
+    private void Start() {
+        updateCameraPriorities();
+    }
+
     private void Update() {
         if(Input.GetButtonDown("NextCam")) {
-            Debug.Log("Right mouse button");
+            currentCamera++;
+            if(currentCamera >= cameras.Length) {
+                currentCamera = 0;
+            }
+            Debug.Log("Current camera is " + currentCamera);
+
+            updateCameraPriorities();
         }
+
         if(Input.GetButtonDown("PrevCam")) {
-            Debug.Log("Left mouse button");
+            currentCamera--;
+            if(currentCamera < 0) {
+                currentCamera = cameras.Length - 1;
+            }
+            Debug.Log("Current camera is " + currentCamera);
+
+            updateCameraPriorities();
+        }
+    }
+
+    private void updateCameraPriorities() {
+        for(int i = 0; i < cameras.Length; i++) {
+            if(i == currentCamera) {
+                cameras[i].Priority = 1;
+            } else {
+                cameras[i].Priority = 0;
+            }
+
+            Debug.Log("Camera " + i + " has priority " + cameras[i].Priority);
         }
     }
 }
