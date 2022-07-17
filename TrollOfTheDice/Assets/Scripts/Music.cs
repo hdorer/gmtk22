82d96c4195a8fabcb2750da3a1c2f9a5a8443a69
Max Cohen <max.cohen@mymail.champlain.edu>
2022-07-17 02:16:09 -0400

    fixed level changes + esc to quit

--- TrollOfTheDice/Assets/Scripts/Music.cs
+++ TrollOfTheDice/Assets/Scripts/Music.cs
@@ -9,4 +9,9 @@ public class Music : MonoBehaviour
     {
         DontDestroyOnLoad(this.gameObject);
     }
+
+    void Update() //esc to quit
+    {
+        if (Input.GetKeyDown("escape")) { Application.Quit(); }
+    }
 }
