77a74d459ecc315a50d1f7c48769172937da8ef3
Max Cohen <max.cohen@mymail.champlain.edu>
2022-07-17 12:47:26 -0400

    i can't even get it to not show up with walls. i'm too tired for this

--- TrollOfTheDice/Assets/Scripts/DicePredictor.cs
+++ TrollOfTheDice/Assets/Scripts/DicePredictor.cs
@@ -6,26 +6,46 @@ public class DicePredictor : MonoBehaviour
 {
     [SerializeField] private float minAlpha;
     [SerializeField] private float maxAlpha;
+    [SerializeField] private float alphaSpeed;
+    [SerializeField] private LayerMask wallLayer;
+
     private SpriteRenderer sr;
+    private bool alphaUp = true;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, minAlpha);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (sr.color.a <= minAlpha) { alphaUp = true; }
+        else if (sr.color.a >= maxAlpha) { alphaUp = false; }
+
+        if (alphaUp) { sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a + alphaSpeed); }
+        else { sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - alphaSpeed); }
+    }
+
+    void Update()
+    {
+        /*if (Physics.Raycast(transform.position, new Vector3(0, 0, -1), 2f, wallLayer))
+        {
+            Debug.Log("here");
+            sr.enabled = false;
+        }
+        else { sr.enabled = true; }*/
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("here");
         if (collision.gameObject.layer == LayerMask.NameToLayer("Wall")) { sr.enabled = false; }
     }
 
-    void OnCollisionExit()
+    void OnTriggerExit()
     {
         sr.enabled = true;
     }
